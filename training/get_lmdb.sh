f7a192995b3902b1a2f620a9273991ffb9e47c2d
Zhe Cao <zhe.cao1993@gmail.com>
2017-01-10 22:57:30 +0800

    Create get_lmdb.sh

---
+++ training/get_lmdb.sh
@@ -0,0 +1,3 @@
+mkdir lmdb_trainVal
+wget -nc --directory-prefix=lmdb_trainVal/ 		http://posefs1.perception.cs.cmu.edu/Users/ZheCao/lmdb_trainVal/data.mdb
+wget -nc --directory-prefix=lmdb_trainVal/ 		http://posefs1.perception.cs.cmu.edu/Users/ZheCao/lmdb_trainVal/lock.mdb

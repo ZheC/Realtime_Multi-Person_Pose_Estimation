48d2cfe0852339c8d4b483c7389fd5402317ed09
Zhe Cao <zhe.cao1993@gmail.com>
2017-02-10 20:18:10 -0500

    add code for generating LMDB

--- training/train_pose.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-/home/zhecao/caffe//build/tools/caffe train --solver=pose_solver.prototxt --gpu=$1 --weights=../../../model/vgg/VGG_ILSVRC_19_layers.caffemodel 2>&1 | tee ./output.txt
